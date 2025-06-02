1cf2df247e63016b5ce754add9070156bf4db1e7
AndrewsLgit <andrewsleruth@gmail.com>
2025-06-02 15:27:36 +0200

    commented unused variables

--- Chick n' Fly/Assets/_/Features/Player/Runtime/PlayerCharacter.cs
+++ Chick n' Fly/Assets/_/Features/Player/Runtime/PlayerCharacter.cs
@@ -7,10 +7,13 @@ namespace Player.Runtime
     public class PlayerCharacter : BigBrother
     {
         #region Private Variables
-        //todo: remove rigidbody and use linearVelocity
-        //todo: make player jump towards arrow direction
-        // todo: make arrow stop moving when _isJumping
+        // todo: remove rigidbody and use linearVelocity
+        // todo: make player jump towards arrow direction
+        // todo: make arrow stop moving when _isJumping -> DONE
+        // make arrow disappear after Jumping until Grounded
         // add collider to stick to surfaces
+        
+        // TODO ????? IMPLEMENT STATE MACHINE FOR ANIMATION TRANSITION AND JUMPING,GROUNDED STATES
         private Rigidbody2D _rigidbody;
         private Tween _tween;
         [SerializeField] private GameObject _directionArrowPivot;
@@ -20,10 +23,10 @@ namespace Player.Runtime
         
         [Header("Rotation Values")]
         // [SerializeField] private float _rotationSpeed = 1.2f;
-        [SerializeField] private float _rotationAngleRight = 0;
-        [SerializeField] private float _rotationAngleLeft = 90f;
+        // [SerializeField] private float _rotationAngleRight = 0;
+        [SerializeField] private float _rotationAngle = 90f;
         [SerializeField] private float _rotationDuration;
-        private Vector3 _initialRotation;
+        // private Vector3 _initialRotation;
 
         [Header("Jump Values")] 
         [SerializeField] private float _jumpForce = 10f;
@@ -37,8 +40,8 @@ namespace Player.Runtime
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
-            _initialRotation = transform.localRotation.eulerAngles;
-            _rotationAngleRight = -_rotationAngleLeft;
+            // _initialRotation = transform.localRotation.eulerAngles;
+            // _rotationAngleRight = -_rotationAngle;
         }
 
         private void OnEnable()
@@ -54,6 +57,7 @@ namespace Player.Runtime
 
         private void FixedUpdate()
         {
+            // execute player rotation logic
             if (_isJumping)
             {
                 _tween.Stop();
@@ -70,11 +74,12 @@ namespace Player.Runtime
             {
                 if (_isGrounded && !_isJumping)
                 {
-                    _tween = Tween.Rotation(_directionArrowPivot.transform, endValue: Quaternion.Euler(0, 0, _rotationAngleLeft), duration: _rotationDuration);
+                    _tween = Tween.Rotation(_directionArrowPivot.transform, endValue: Quaternion.Euler(0, 0, _rotationAngle), duration: _rotationDuration);
                     // if (_isJumping || !_isGrounded)
                     // {
                     //     _tween.Stop();
                     // }
+                    // rotate to other side on finish
                     _tween.OnComplete(ArrowRotationRight); 
                     
                 }
@@ -90,19 +95,21 @@ namespace Player.Runtime
 
                 if (_isGrounded && !_isJumping)
                 {
-                    _tween =  Tween.Rotation(_directionArrowPivot.transform, endValue: Quaternion.Euler(0, 0, _rotationAngleRight), duration: _rotationDuration);
+                    // invert _rotationAngle
+                    _tween =  Tween.Rotation(_directionArrowPivot.transform, endValue: Quaternion.Euler(0, 0, -_rotationAngle), duration: _rotationDuration);
                     // if (_isJumping || !_isGrounded)
                     // {
                     //     _tween.Stop();
                     // }
+                    // Rotate to other side on finish
                     _tween.OnComplete(ArrowRotationLeft);
                 }
             }
 
             public void Jump()
             {
+                
                 _rigidbody.AddForce(new Vector3(0, _jumpForce, 0), ForceMode2D.Impulse);
-                //todo: make player jump with _rigidbody.AddForce() with impulse
             }
         
         #endregion

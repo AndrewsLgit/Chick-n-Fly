86d3bd5796eb2d7faf426a04a2b1c5aec26784ef
AndrewsLgit <andrewsleruth@gmail.com>
2025-06-03 04:03:18 +0200

    Added GroundChecker script (to detect collision to platforms)
    TODO: make player stick to platforms on isGrounded

---
+++ Chick n' Fly/Assets/_/Features/Player/Runtime/GroundChecker.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Player.Runtime
+{
+    public class GroundChecker : BigBrother
+    {
+        [SerializeField] float _groundDistance = 0.1f;
+        [SerializeField] LayerMask _groundLayers;
+        
+        public bool IsGrounded { get; private set; }
+
+        private void Update() {
+            // changed direction: to new Vector 3 right and up
+            IsGrounded = Physics.SphereCast(transform.position, _groundDistance, new Vector3(1,1,0), out var _ , _groundDistance, _groundLayers);
+            if (IsGrounded) Info($"Currently grounded: {IsGrounded}");
+        }
+    } 
+}
